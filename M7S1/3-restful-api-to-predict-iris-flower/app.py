289a70c99bc3d062cf35fc3162c691b42f940f6e
Rahmafatin Nurul Izza <rhmftn.ni@gmail.com>
2022-06-07 11:43:15 +0700

    Initial commit

--- M7S1/3-restful-api-to-predict-iris-flower/app.py
+++ M7S1/3-restful-api-to-predict-iris-flower/app.py
@@ -5,27 +5,43 @@ import numpy as np
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def iris_prediction():
     if request.method == 'GET':
-        return render_template("iris-prediction.html")
+        return render_template('iris-prediction.html')
     elif request.method == 'POST':
+        print('OK')
         print(dict(request.form))
-        iris_features = dict(request.form).values()
-        iris_features = np.array([float(x) for x in iris_features])
-        model, std_scaler = joblib.load("model-development/iris-classification-using-logistic-regression.pkl")
-        iris_features = std_scaler.transform([iris_features])
-        print(iris_features)
-        result = model.predict(iris_features)
-        iris = {
-            '0': 'Iris Setosa',
-            '1': 'Iris Versicolor',
-            '2': 'Iris Virginica'
+
+        heart_features = dict(request.form).values()
+        heart_features = np.array([float(x) for x in heart_features])
+        model, std_scaler = joblib.load('model/Hearth.pkl')
+        heart_features = std_scaler.transform([heart_features])
+        print(heart_features)
+
+        result = model.predict(heart_features)
+        heart = {
+            '0': 'Engga',
+            '1': 'Iyaa',
         }
-        result = iris[str(result[0])]
+        result = heart[str(result[0])]
         return render_template('iris-prediction.html', result=result)
+
+        # iris_features = dict(request.form).values()
+        # iris_features = np.array([float(x) for x in iris_features])
+        # model, std_scaler = joblib.load('model-development/iris-classification-using-logistic-regression.pkl')
+        # iris_features = std_scaler.transform([iris_features])
+        # print(iris_features)
+        # result = model.predict(iris_features)
+        # iris = {
+        #     '0': 'Iris Setosa',
+        #     '1': 'Iris Versicolor',
+        #     '2': 'Iris Virginica'
+        # }
+        # result = iris[str(result[0])]
+        # return render_template('iris-prediction.html', result=result)
     else:
-        return "Unsupported Request Method"
+        return 'Unsupported Request Method'
 
 
 if __name__ == '__main__':
